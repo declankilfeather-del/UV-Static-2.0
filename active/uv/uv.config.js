d48c554bb08d54bd0c57a590ec607f814e12ffd8
declankilfeather-del <declankilfeather@gmail.com>
2026-03-21 23:58:13 -0400

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,17 +1,11 @@
-/* 
-   BlackCosmos Tunnel Configuration
-   Using Public Bare Server to bypass Vercel Static Limits
-*/
 self.__uv$config = {
     prefix: '/active/service/',
-    
-    // PUBLIC TUNNEL (Stops the Bare-Mux Error)
+    // This Bare Server is configured to bypass CORB blocks
     bare: 'https://v.m9.io', 
-    
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/active/uv/uv.handler.js',
     bundle: '/active/uv/uv.bundle.js',
     config: '/active/uv/uv.config.js',
-sw: '/active/sw.js',
+    sw: '/active/sw.js',
 };
